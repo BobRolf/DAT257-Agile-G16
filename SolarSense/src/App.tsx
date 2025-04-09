918a326fcf24329e5ddcc6054ee59e0c09aea06d
simon <simon.h.bergstrom@gmail.com>
2025-04-09 15:02:48 +0200

    Google API imported and added environment variables to gitignore

--- SolarSense/src/App.tsx
+++ SolarSense/src/App.tsx
@@ -1,7 +1,34 @@
-import Title from './components/Title'
+"use client";
 
-function App(){
-  return <div><Title></Title></div>
+import Title from "./components/Title";
+import {
+  //Tools from google api lmao
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
+
+function App() {
+  const position = { lat: 53.54, lng: 10 };
+
+  return (
+    <>
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        {" "}
+        {/* Get API-key from environment variable*/}
+        <div>
+          <Title></Title>
+        </div>
+        {/* EXAMPLE MAP CODE
+        <div style={{ height: "100vh", width: "100%" }}>
+          <Map zoom={9} center={position}></Map>
+        </div>
+        */}
+      </APIProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
