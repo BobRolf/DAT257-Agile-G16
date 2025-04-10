4a17a988d6c8761a07628fbfb0a989c6a387d129
simon <simon.h.bergstrom@gmail.com>
2025-04-10 22:13:09 +0200

    Added searchbox which filters adderesses and only in Sweden

--- SolarSense/src/App.tsx
+++ SolarSense/src/App.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Title from "./components/Title";
+import Autocomplete from "react-google-autocomplete";
 import {
   //Tools from google api lmao
   APIProvider,
@@ -15,18 +16,22 @@ function App() {
 
   return (
     <>
-      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-        {" "}
-        {/* Get API-key from environment variable*/}
-        <div>
-          <Title></Title>
-        </div>
-        {/* EXAMPLE MAP CODE
-        <div style={{ height: "100vh", width: "100%" }}>
-          <Map zoom={9} center={position}></Map>
-        </div>
-        */}
-      </APIProvider>
+      {/* Get API-key from environment variable*/}
+      <div>
+        <Title></Title>
+      </div>
+      <div>
+        <Autocomplete
+          apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+          onPlaceSelected={(place) => {
+            console.log(place);
+          }}
+          options={{
+            types: ["address"], // 🏡 Endast adresser (inte företag/städer)
+            componentRestrictions: { country: "se" }, // 🇸🇪 Bara Sverige (valfritt)
+          }}
+        ></Autocomplete>
+      </div>
     </>
   );
 }
