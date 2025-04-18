f806f722d90e38280ef492e74da7ca633f8720ed
simon <simon.h.bergstrom@gmail.com>
2025-04-18 21:30:02 +0200

    Update LocationSelector.tsx
    removed unecessary dependencies

--- SolarSense/src/components/LocationSelector.tsx
+++ SolarSense/src/components/LocationSelector.tsx
@@ -1,9 +1,7 @@
-import { useRef } from "react";
 import AddressFinder from "./AddressFinder";
 import CoordinateInput from "./CoordinateInput";
 import MapComponent from "./MapComponent";
 import GoogleAPILoader from "./GoogleAPILoader";
-libraries: ["places"];
 
 function LocationSelector() {
   return (
