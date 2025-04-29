f759ef57236373da0c03c040bfc2ba919fbe6a12
Bob Rolf <143784071+BobRolf@users.noreply.github.com>
2025-04-29 12:49:48 +0200

    added facts

--- SolarSense/src/pages/Facts.tsx
+++ SolarSense/src/pages/Facts.tsx
@@ -6,7 +6,16 @@ const Facts: React.FC = () => {
   return (
     <div>
       <Navbar /> {/* Add Navbar */}
-      <h1>Facts</h1>
+      <div className="container mt-5 d-flex justify-content-center">
+        <h1>Facts</h1>
+      </div>
+      <h5
+        className="text-center"
+        style={{ lineHeight: "1.5", maxWidth: "700px", margin: "0 auto" }}
+      >
+        In this page we will display some facts and information about solar
+        panels and their impact on the environment.
+      </h5>
       <div className="container mt-5 d-flex justify-content-center">
         <FactsCard /> {/* Add AboutCard */}
       </div>
