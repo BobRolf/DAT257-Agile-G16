65c10274f3eda37aa416c51656d8b6a4d69d7337
simon <simon.h.bergstrom@gmail.com>
2025-05-02 15:11:00 +0200

    Update Footer.tsx
    Replaced regular links with navlinks

--- SolarSense/src/components/Footer.tsx
+++ SolarSense/src/components/Footer.tsx
@@ -1,31 +1,38 @@
+import { NavLink } from "react-router-dom";
+
 function Footer() {
   return (
     <footer className="py-1">
       <ul className="nav justify-content-center border-bottom py-1 mb-3">
         <li className="nav-item">
-          <a href="/" className="nav-link px-2 text-body-secondary">
+          <NavLink className="nav-link px-2 text-body-secondary" to="/">
             Home
-          </a>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a href="/Calculator" className="nav-link px-2 text-body-secondary">
+          <NavLink
+            className="nav-link px-2 text-body-secondary"
+            to="/Calculator"
+          >
             Calculator
-          </a>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a href="/Facts" className="nav-link px-2 text-body-secondary">
+          <NavLink className="nav-link px-2 text-body-secondary" to="/Facts">
             Facts
-          </a>
+          </NavLink>
         </li>
+
         <li className="nav-item">
-          <a href="/About" className="nav-link px-2 text-body-secondary">
+          <NavLink className="nav-link px-2 text-body-secondary" to="/About">
             About
-          </a>
+          </NavLink>
         </li>
+
         <li className="nav-item">
-          <a href="/Settings" className="nav-link px-2 text-body-secondary">
+          <NavLink className="nav-link px-2 text-body-secondary" to="/settings">
             Settings
-          </a>
+          </NavLink>
         </li>
       </ul>
       <p className="text-center text-body-secondary">© 2025 SolarSense</p>
